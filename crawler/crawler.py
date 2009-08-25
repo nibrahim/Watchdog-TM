42565088a70e1c9456ae9f42c172608c75ca7ffc
Noufal Ibrahim <noufal@nibrahim.net.in>
2009-08-26 00:30:55 +0530

    Print out actual values

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -5,9 +5,38 @@ from xml.etree.cElementTree import ElementTree
 def parse(f):
     tree = ElementTree()
     tree.parse(f)
-    m = tree.findall("application-information/file-segments/action-keys/case-file")
-    print m
+    for node in tree.findall("application-information/file-segments/action-keys/case-file"):
+        print "-------------------", node,
+        for element in node.getchildren():
+            print element.tag.strip(),
+            if element.getchildren():
+                for i in element.getchildren():
+                    print "" 
+                    print "  ",i.tag.strip(), i.text.strip()
+            else:
+                print element.text.strip()
 
 if __name__ == "__main__":
-    # parse("sample_data/daily/sample.xml")
-    parse("sample_data/daily/apc090101.xml")
+    parse("sample_data/daily/sample.xml")
+    # parse("sample_data/daily/apc090101.xml")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
