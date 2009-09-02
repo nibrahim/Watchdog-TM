e069ef0eb9b521c009487fe837243d0bcb2fb14d
Noufal Ibrahim <noufal@nibrahim.net.in>
2009-09-03 01:40:51 +0530

    Minor elementtree changes

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -8,14 +8,15 @@ def parse(f):
     for node in tree.findall("application-information/file-segments/action-keys/case-file"):
         print "-------------------", node,
         for element in node.getchildren():
-            print element.tag.strip(),
-            if element.getchildren():
-                for i in element.getchildren():
-                    print "" 
-                    print "  ",i.tag.strip(), i.text.strip()
-            else:
-                print element.text.strip()
+            print element.tag.strip()
+            # if element.getchildren():
+            #     for i in element.getchildren():
+            #         print "" 
+            #         print "  ",i.tag.strip(), i.text.strip()
+            # else:
+            #     print element.text.strip()
 
+        
 if __name__ == "__main__":
     parse("sample_data/daily/sample.xml")
     # parse("sample_data/daily/apc090101.xml")
