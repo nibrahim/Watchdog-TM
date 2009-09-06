0b5aaf54fb64b04199d726b7b82c9d7afc431dde
Noufal Ibrahim <noufal@nibrahim.net.in>
2009-09-07 01:34:27 +0530

    Added a few more fields

--- crawler/tables.sql
+++ crawler/tables.sql
@@ -1,4 +1,9 @@
--- Based on Trademark-Assignments-Documentation-v0.4.pdf
+-- Based on Trademark-Applications-Documentation-v2.0-07152005.pdf
 CREATE TABLE trademarks (
+       action_key		varchar(2),	-- Action key for type of event
+       serial_number		varchar(8),     -- Case serial number
+       registration-number	varchar(7),     -- Registration no.
+       
+
        
 );
