c5a858e002146deedb2f61be96d9913a35852936
Noufal Ibrahim <noufal@synopsys.COM>
2009-10-27 22:59:00 +0530

    Parsed header and statements nodes

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -40,25 +40,66 @@ def extract_fields(node,fields):
             val = n.text.strip()
             info[i] = val
     return info
-    
+
+def extract_case_file_statements(node):
+    """Extracts all <case-file-statement> nodes from the given 'node' and
+    returns them in a list of dictionaries"""
+    ret = []
+    if not node:
+        return ret
+    for i in node.findall("case-file-statement"):
+        typ  = i.find("type-code").text.strip()
+        text = i.find("text").text.strip()
+        ret.append({'type-code': typ, 'text' : text})
+    return ret
+
+def extract_case_file_event_statements(node):
+    """Extracts all <case-file-event-statement> nodes from the given 'node' and
+    returns them in a list of dictionaries"""
+    ret = []
+    if not node:
+        return ret
+    for i in node.findall("case-file-event-statement"):
+        code = i.find("code").text.strip()
+        typ  = i.find("type").text.strip()
+        desc = i.find("description-text").text.strip()
+        date = i.find("date").text.strip()
+        number = i.find("number").text.strip()
+        ret.append({'code'   : code,
+                    'type'   : typ,
+                    'desc'   : desc,
+                    'date'   : date,
+                    'number' : number
+                    })
+    return ret
+
 
 def parse(f):
     tree = ElementTree()
     tree.parse(f)
     count = 0
     for node in tree.findall("application-information/file-segments/action-keys/case-file"):
+        # Basic unique identification information
         ident = extract_fields(node,_case_file_identification_fields)
         print ident
+        # Header information
         header = extract_fields(node.find("case-file-header"),_case_file_header_fields)
+        print " Headers:"
         for i,j in header.iteritems():
-            print "  %-40s  %s"%(i,j)
-            # if element.getchildren():
-            #     for i in element.getchildren():
-            #         print "" 
-            #         print "  ",i.tag.strip(), i.text.strip()
-            # else:
-            #     print element.text.strip()
+            print "   %-40s  %s"%(i,j)
+        # Statements
+        statements = extract_case_file_statements(node.find("case-file-statements"))
+        print " Statements:"
+        for k in statements:
+            print "   - %-40s  %s"%(k['type-code'],k['text'])
+        # Events
+        events = extract_case_file_event_statements(node.find("case-file-event-statements"))
+        print " Events:"
+        for k in events:
+            print "   - %4s %1s %50s %8s %3s"%(k['code'],k['type'],k['desc'],k['date'],k['number'])
+            
 
+        print 80*"="
         
 if __name__ == "__main__":
     logging.basicConfig(level = logging.DEBUG, format="[%(lineno)d:%(funcName)s] - %(message)s")
